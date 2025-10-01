9bd8a359b7a9925fa8537e5321d70522b3a69b9f
Santoshb22 <sb289848@gmail.com>
2025-10-01 15:23:24 +0530

    write login controller and generate token function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,17 @@
 const User = require("../models/user.model");
 const { uploadtoCloudinary } = require("../utils/cloudinary");  
 
+const generateAccessAndRefreshTokens = async (userId) => {
+    const user = await User.findbyId(userId);
+    const accessToken = await user.generateAccessToken();
+    const refreshToken = await user.generateRefreshToken();
+
+    User.refreshToken = refreshToken;
+    await user.save({validateBeforeSave: false});
+
+    return { accessToken, refreshToken };
+} 
+
 const register = async (req, res) => {
     try {
         const { name, username, email, password } = req.body;
@@ -54,4 +65,50 @@ const register = async (req, res) => {
     }
 }
 
-module.exports = { register };
+const login = async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        if(!email && !username) {
+            return res.status(400).json({ message: "Email or username is required" });
+        }
+
+        const user = await user.findOne({
+            $or: [ { email }, { username } ]
+        })
+
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if(!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password" });
+        }
+        const isPasswordValid = await user.isPasswordMatch(password);
+
+        const {refreshToken, accessToken} = await generateAccessAndRefreshTokens(user._id);
+        const loggedInUser = await User.findbyId(user._id).select("-password -refreshToken");
+
+        const cookieOptions = {
+            httpOnly: true,
+            secure: true,
+        };
+
+        return res.status(200)
+        .cookie("refreshToken", refreshToken, cookieOptions)
+        .cookie("accessToken", accessToken, cookieOptions)
+        .json({
+            success: true,
+            message: "Login successful",
+            accessToken,
+            user: loggedInUser
+        });
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ message: "Server error during login" }); 
+    }
+}
+
+module.exports = { 
+    register,
+    login
+};
