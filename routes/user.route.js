fcce1ed5a866d9dd3167bee16896be3cbd8e8d59
Santoshb22 <sb289848@gmail.com>
2025-10-02 15:26:30 +0530

    add 3 new routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const upload = require('../middlewares/multer.middleware');
-const { login, register, logout, generateNewRefreshToken } = require('../controllers/auth.controller');
+const { login, register, logout, generateNewRefreshToken, editProfile, editAvatar, getProfile } = require('../controllers/auth.controller');
 const verifyToken = require('../middlewares/auth.middleware');
 
 const router = express.Router();
@@ -9,6 +9,8 @@ router.post('/register', upload.single("avatar"), register);
 router.post('/login', login);
 router.post('/logout',verifyToken, logout);
 router.post('/refresh-token', generateNewRefreshToken);
-
+router.put('/edit-profile', verifyToken, editProfile); 
+router.put('/edit-avatar', verifyToken, upload.single("avatar"), editAvatar);   
+router.get('/profile', verifyToken, getProfile);;
 
 module.exports = router;
