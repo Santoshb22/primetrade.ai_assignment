b1305f4136c782fc78e81735d90b69b865f5fd53
Santoshb22 <sb289848@gmail.com>
2025-10-02 10:52:24 +0530

    implement conditional rendering component based on authstatus and username on header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import Auth from './page/Auth'
-import Dashboard from './page/Dashboard'
-import Header from './component/Header'
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router';
+import Auth from './page/Auth';
+import Dashboard from './page/Dashboard';
+import Header from './component/Header';
 
 const App = () => {
+  const authStatus = JSON.parse(localStorage.getItem("userState"))?.authStatus;
+
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={authStatus ? <Navigate to="/dashboard" /> : <Auth />} />
+        <Route path="/dashboard" element={authStatus ? <Dashboard /> : <Navigate to="/" />} />
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Header = () => {
+    const userName = useSelector(store => store?.user?.userInfo?.username);
+    console.log(userName);
   return (
     <div className='flex justify-between p-4 bg-indigo-100'>
         <div className='flex gap-2 items-center cursor-pointer'>
@@ -12,13 +15,16 @@ const Header = () => {
         </div>
 
         <div className='flex gap-4 items-center'>
-            <p className='text-lg font-medium cursor-pointer'>
-                santoshb22
-            </p>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-            </svg>
-
+            {userName && (
+                <>
+                    <p className='text-lg font-medium cursor-pointer'>
+                        {userName}
+                    </p>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                        <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
+                    </svg>
+                </>
+            )}
         </div>
     </div>
   )
